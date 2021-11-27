cfe2fe486db817dd10783de7bf16f95274447bd3
Danish1111ansari <90023391+Danish1111ansari@users.noreply.github.com>
2021-11-27 22:21:14 +0530

    Add files via upload

---
+++ assingment_js/script.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(firstName, lastName, officeAdress) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.officeAdress = officeAdress;
+  }
+}
+var person1 = new Person("JHON", "DOE", "patel chowk,bhabua");
+var person2 = new Person("DANISH", "ANSARI", "Ekta chowk,bhabua");
+var person3 = new Person("AYUSH", "KUMAR", "Nitp,patna");
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
